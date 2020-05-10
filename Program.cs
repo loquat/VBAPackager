45c58101e39c8a215daec303a5b1b147f9d5df2a
Sancarn <sancarn@hotmail.co.uk>
2020-05-10 01:43:06 +0100

    Test program
    At the moment mainly for debugging.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using VBAPackager;
+
+class Program {
+    static void Main(string[] args) {
+        string pth = "Test.xlsm";
+        Console.WriteLine("Decompressing " + pth);
+        VBAProject x = new VBAProject(pth);
+        return;
+    }
+}
+
